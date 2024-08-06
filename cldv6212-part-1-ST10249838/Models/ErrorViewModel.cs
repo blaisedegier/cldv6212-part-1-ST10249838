00e6375970ad7da2e4b17fe053a374a534c96468
Blaise Mikka de Gier <ST10249838@vcconnect.edu.za>
2024-08-06 19:32:46 +0200

    Add project files.

---
+++ cldv6212-part-1-ST10249838/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cldv6212_part_1_ST10249838.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
