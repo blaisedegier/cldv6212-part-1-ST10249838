320e9bbad3c6bd7b10de9b2f577dacf59b540fbe
Blaise Mikka de Gier <ST10249838@vcconnect.edu.za>
2024-08-28 00:58:56 +0200

    Add custom attribute for file extension validation
    Introduce `AllowedExtensionsAttribute` in `Part1.Attributes` to validate file extensions in ASP.NET Core. The attribute is applied to properties and ensures only specified extensions are allowed. Validation logic is implemented in the `IsValid` method. Attribution comments included.

---
+++ cldv6212-part-1-ST10249838/Attributes/AllowedExtensionsAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+/*
+ * Code Attribution:
+ * Custom Attributes
+ * tdykstra
+ * 9 September 2023
+ * learn.microsoft.com
+ * https://learn.microsoft.com/en-us/aspnet/core/mvc/models/validation?view=aspnetcore-6.0#custom-attributes
+ */
+namespace Part1.Attributes
+{
+    // This class is used to validate the file extension of the uploaded file.
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class AllowedExtensionsAttribute : ValidationAttribute
+    {
+        private readonly string[] _extensions;
+        public AllowedExtensionsAttribute(string[] extensions)
+        {
+            _extensions = extensions;
+        }
+
+        protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+            if (file != null)
+            {
+                var extension = Path.GetExtension(file.FileName).ToLowerInvariant();
+                if (!_extensions.Contains(extension))
+                {
+                    return new ValidationResult($"Only {string.Join(", ", _extensions)} files are allowed.");
+                }
+            }
+
+            return ValidationResult.Success!;
+        }
+    }
+}
